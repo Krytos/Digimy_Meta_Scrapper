893ef5c53794675d2817cb66e4c691dcdad6cab2
Krytos <kmeinon@gmail.com>
2023-01-30 18:33:25 +0100

    - Improved speed by removing tasks for dead links

--- main.py
+++ main.py
@@ -10,61 +10,59 @@ from bs4 import BeautifulSoup
 
 
 async def fetch_metadata(session, item_link: str, data):
-    async with session.get(item_link) as response:
+    try:
+        async with session.get(item_link) as response:
+            soup = BeautifulSoup(await response.text(), 'html.parser')
+            page_title = soup.find("meta", attrs={"property": "og:title"})["content"].replace("&nbsp;", " ").replace(
+                '"',
+                "'")
+            print(f"Fetching metadata for {page_title}...")
+            page_description = soup.find("meta", attrs={"name": "description"})["content"].replace("&nbsp;",
+                                                                                                   " ").replace(
+                '"', "'")
+            produkt_beschreibung = [p.text for p in soup.find('div', class_='vsLeft')]
+            produkt_beschreibung = [p.strip().replace("&nbsp;", " ").replace('"', "'") for p in produkt_beschreibung if
+                                    p != ' ']
+            produkt_beschreibung = " ".join(produkt_beschreibung[1:-3]).replace("&nbsp;", " ").replace("&nbsp", " ")
+            data.append(
+                {
+                    "meta title": page_title,
+                    "meta description": page_description,
+                    "produkt beschreibung": produkt_beschreibung
+                }
+            )
+            try:
+                my_bar.progress(int(100 / total_tasks * (total_tasks - len(asyncio.all_tasks()) + 1)))
+            except Exception:
+                pass
+            await asyncio.sleep(0.01)
+    except Exception as e:
+        print(e)
+        pass
+
+
+async def fetch_pages_for_link(session, url: str, tg: asyncio, data):
+    async with session.get(url) as response:
+        print(f"Fetching {url}...")
         soup = BeautifulSoup(await response.text(), 'html.parser')
-        page_title = soup.find("meta", attrs={"property": "og:title"})["content"].replace("&nbsp;", " ").replace('"',
-                                                                                                                 "'")
-        page_description = soup.find("meta", attrs={"name": "description"})["content"].replace("&nbsp;", " ").replace(
-            '"', "'")
-        produkt_beschreibung = [p.text for p in soup.find('div', class_='vsLeft')]
-        produkt_beschreibung = [p.strip().replace("&nbsp;", " ").replace('"', "'") for p in produkt_beschreibung if
-                                p != ' ']
-        produkt_beschreibung = " ".join(produkt_beschreibung[1:-3]).replace("&nbsp;", " ").replace("&nbsp", " ")
-        data.append(
-            {
-                "meta title": page_title,
-                "meta description": page_description,
-                "produkt beschreibung": produkt_beschreibung
-            }
-        )
         try:
-            my_bar.progress(int(100 / total_tasks * (total_tasks - len(asyncio.all_tasks()) + 1)))
-        except Exception:
+            items = soup.find('div', class_='vscenter').contents
+            if len(items) > 1:
+                for item in [item for item in items if item != ' ']:
+                    tg.create_task(fetch_metadata(session, item.find('a')['href'], data))
+                    print(f"Created task: Fetchingitem {item.find('a')['href']}")
+                    await add_tasks()
+            else:
+                current_task = asyncio.current_task().get_name()
+                for task in [task for task in asyncio.all_tasks() if int(task.get_name()[4:]) > int(current_task[4:])]:
+                    task.cancel()
+        except AttributeError:
             pass
 
 
-async def fetch_items(session, url: str, tg: asyncio, data):
+async def add_tasks(n=1):
     global total_tasks
-    async with session.get(url) as response:
-        soup = BeautifulSoup(await response.text(), 'html.parser')
-        listings = soup.find('div', class_='listing')
-        try:
-            content = [x for x in listings.children][1].contents
-        except AttributeError:
-            return
-        if len(content) == 1:
-            return
-
-        for item in content:
-            try:
-                item_link = item.find('a')['href']
-                tg.create_task(fetch_metadata(session, item_link, data))
-                await add_tasks()
-            except TypeError:
-                continue
-
-
-async def fetch_pages_for_link(session, link: str, tg: asyncio, data):
-    for i in range(1, pages_to_scrap + 1):
-        url = link + '?p=' + str(i)
-        tg.create_task(fetch_items(session, url, tg, data))
-        await add_tasks()
-
-
-async def add_tasks():
-    global total_tasks
-    total_tasks += 1
-
+    total_tasks += n
 
 async def main():
     start = perf_counter()
@@ -82,12 +80,20 @@ async def main():
                 links = []
                 for i, category in enumerate(url_extensions[1]):
                     if category in categories_to_scrap:
-                        links.append(base_url + url_extensions[0][i])
+                        if category == "Rebsorten":
+                            async with session.get(base_url + url_extensions[0][i]) as reb:
+                                soup = BeautifulSoup(await reb.text(), 'html.parser')
+                                sub_links = [a.find("a")["href"] for a in
+                                             soup.find_all('div', class_='vscategorylistingitem')]
+                                links.extend(sub_links)
+                        else:
+                            links.append(base_url + url_extensions[0][i])
 
             async with asyncio.TaskGroup() as tg:
-                for link in links:
-                    tg.create_task(fetch_pages_for_link(session, link, tg, data))
-                await asyncio.sleep(0.5)
+                for link in [link + '?p=' + str(i) for i in range(1, 100) for link in links]:
+                    tg.create_task((fetch_pages_for_link(session, link, tg, data)), name=f'task{link.split("?p=")[1]}')
+                    print(f"Created task {link.split('?p=')[1]}: Generating link for {link}")
+                    await add_tasks()
             with open(f"{datei_name}.json", "w+", encoding='utf-8') as f:
                 json.dump(data, f, ensure_ascii=False, indent=4)
             with open(f"{datei_name}.csv", "w", encoding='utf-8') as f:
@@ -97,6 +103,7 @@ async def main():
             while not os.path.exists(f"{datei_name}.json") and not os.path.exists(f"{datei_name}.csv"):
                 await asyncio.sleep(0.1)
     print(f"Finished in {perf_counter() - start} seconds")
+
 
 total_tasks = 0
 base_url = "https://www.vinoscout.de/"
@@ -109,13 +116,12 @@ categories_to_scrap = st.multiselect("Kategoregien zu scrappen",
                                      ["Rotwein", "Weißwein", "Roséwein", "Schaumwein", "Portwein", "Sherry",
                                       "Spirituosen", "Feinkost", "Cidre", "Alkoholfreie Getränke", "Länder",
                                       "Rebsorten"])
-# categories_to_scrap = ["Spirituosen"]
+
 pages_to_scrap = st.slider("Pages to scrap", 1, 100, 50)
-# pages_to_scrap = 50
 datei_name = st.text_input("Dateiname", "results")
 my_bar = st.progress(0)
-# asyncio.run(main())
-button = st.button("Start scrapping", on_click=lambda: asyncio.run(main()))
+button = st.button("Start scrapping", on_click=asyncio.run, args=(main(),))
+
 try:
     with open(f"{datei_name}.json", "r", encoding='utf-8') as f:
         st.download_button('Download JSON', f, f"{datei_name}.json")
